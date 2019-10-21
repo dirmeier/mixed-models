Fix first bug in computation of working weights

lme/ls.py
@@ -1,5 +1,6 @@
-import jax.scipy as sp
-from jax.scipy.linalg import inv
+import numpy as np
+from numpy.linalg import inv
+
 
 def wls(y, X, W):
     XT_Winv = X.T.dot(inv(W))
@@ -14,6 +15,23 @@ def working_response(y, X, U, beta, gamma, invlink, grad):
     eta = X.dot(beta) + U.dot(gamma)
     mean = invlink(eta)
     deriv = grad(eta)
-    Dinv = sp.diag(1 / deriv)
+    Dinv = np.diag(1 / deriv)
     working = eta + Dinv.dot(y - mean)
     return working
+
+
+def working_weight(y, X, U, beta, gamma, invlink, grad):
+    eta = X.dot(beta) + U.dot(gamma)
+    return grad(eta)**2 / invlink(eta)
+
+
+def irls(X, U, G, w, y):
+    
+    C = np.hstack([X, U])
+    W = np.diag(w)
+    CTW = C.T.dot(W)
+    CTWC = CTW.dot(C)
+    B = np.zeros_like(CTWC)
+    B[(B.shape[0] - G.shape[0]):, (B.shape[1] - G.shape[1]):] = np.linalg.inv(G)
+    est = np.linalg.inv(CTWC + B).dot(C.T).dot(W).dot(y)
+    return est[:X.shape[1]] , est[X.shape[1]:]

main.py
@@ -1,14 +1,12 @@
 import scipy as sp
-import jax.scipy as jsp
 import pandas
 import pandas as pd
 from patsy import dmatrices
 import scipy.stats as st
 from sklearn.preprocessing import LabelEncoder
-from jax import grad
 
 from lme.optim import optim
-from lme.ls import wls, solve_gamma, working_response
+from lme.ls import wls, solve_gamma, working_response, working_weight, irls
 from lme.marginal_likelhood import restricted_mll, profile_mll
 from lme.util import block_diag, as_ranef_cov, v
 
@@ -29,7 +27,7 @@ def _build_ranef_model_matrix(tab, factor, ranef):
     Z = pd.concat([inter_tab, slope_tab], axis=1, sort=True)
     Z = Z.reindex(sorted(Z.columns, key=lambda x: x[1]), axis=1)
 
-    return Z.values
+    return sp.asarray(Z.values)
 
 
 def lme():
@@ -45,7 +43,7 @@ def lme():
                 cov=sp.diag(sd * sp.ones(n)))
 
     pll = restricted_mll("gaussian")
-    fn = lambda nu, y, X, U: jsp.asscalar(pll(nu, y, X, U)[0])
+    fn = lambda nu, y, X, U: sp.asscalar(pll(nu, y, X, U)[0])
     optimz = optim(fn, y, X, U)
     sd_hat, nu_hat = optimz['sigma'], optimz['nu']
 
@@ -58,6 +56,8 @@ def lme():
 
     print("beta/beta_hat:\n{}/{}\n".format(beta, b_hat))
     print("gamma/gammahat:\n{}/\n{}\n".format(gamma, gamma_hat))
+
+
 
 
 def glme():
@@ -77,19 +77,17 @@ def glme():
     b_tilde = bold = sp.ones(shape=p)
     g_tilde = gold = sp.ones(shape=q * 2)
     
-    invlink = sp.exp
-    gradinvlink = grad(invlink)
-    
     while True:
-        y_tilde = working_response(y, X, U, b_tilde, g_tilde, invlink, gradinvlink)
+        y_tilde = working_response(y, X, U, b_tilde, g_tilde, sp.exp, sp.exp)
+        w_tilde = working_weight(y, X, U, b_tilde, g_tilde, sp.exp, sp.exp)
+        # TODO V is computed differently here ->? bug
         optimz = optim(fn, y_tilde, X, U, iter=1)
         sd_hat, nu_hat = optimz['sigma'], optimz['nu']
-        V_hat, G_hat, R_hat = v(sd_hat, nu_hat, n, q, U)
-        b_tilde = wls(y_tilde, X, V_hat)
-        g_tilde = solve_gamma(y_tilde, X, G_hat, U, V_hat, b_tilde)
+        _, G_hat, _ = v(sd_hat, nu_hat, n, q, U)
+        b_tilde, g_tilde = irls(X, U, G_hat, w_tilde, y_tilde)
 
-        if sp.sum((b_tilde - bold) ** 2) < 0.0001 and \
-           sp.sum((g_tilde - gold) ** 2) < 0.0001:
+        if sp.sum((b_tilde - bold) ** 2) < 0.001 and \
+           sp.sum((g_tilde - gold) ** 2) < 0.001:
            break
         bold, gold = b_tilde, g_tilde
 
@@ -103,10 +101,9 @@ def glme():
 
 if __name__ == "__main__":
     import scipy
-    scipy.random.seed(42)
+    scipy.random.seed(2)
     tab = pandas.read_csv("./data/sleepstudy.csv")
     _, X = dmatrices("Reaction~ Days", tab)
     X = sp.asarray(X)
     U = _build_ranef_model_matrix(tab, "Subject", "Days")
-
     glme()